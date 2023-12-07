ffd4849d821822a6020f96f10f1494613897b5f8
Marko <mskender@schubergphilis.com>
2023-12-07 09:36:31 +0100

    first pass

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
 
-    prefix = var.prefix == "" ?  "" : "${var.prefix}-"
-
+  prefix                   = var.prefix == "" ? "" : "${var.prefix}-"
+  eks_worker_iam_role_name = var.karpenter_worker_iam_role_name == "" ? "${local.prefix}-${var.eks_cluster_name}-worker-group" : var.karpenter_worker_iam_role_name
 }
